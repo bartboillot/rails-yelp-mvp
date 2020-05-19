503375197730beae08c7c56b9cb998da8fb2341d
bart boillot <barthboillot@hotmail.com>
2020-05-19 20:43:42 +0200

    yelp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+10.times do
+  restaurant = Restaurant.new
+  restaurant.name = Faker::Restaurant.name
+  restaurant.address = Faker::Address.full_address
+  restaurant.phone_number = Faker::PhoneNumber.cell_phone_with_country_code
+  restaurant.category = ["chinese", "italian", "japanese", "french", "belgian"].sample
+  restaurant.save
+end
+puts 'Done!'
